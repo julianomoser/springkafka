5b554e691e498fab0bec18f5a8433fd06dd83917
Juliano Moser <juliano.moserjj@gmail.com>
2022-02-07 01:19:29 -0300

    Criação do projeto

---
+++ src/test/java/br/com/moser/springkafka/SpringkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moser.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
