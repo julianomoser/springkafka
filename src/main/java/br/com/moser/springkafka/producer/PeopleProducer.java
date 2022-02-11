ae9f62d0e9fc410169c22d34eaf78435a0c44da7
Juliano Moser <juliano.moserjj@gmail.com>
2022-02-10 23:11:47 -0300

    Implementado Produtor

---
+++ src/main/java/br/com/moser/springkafka/producer/PeopleProducer.java
@@ -0,0 +1,31 @@
+package br.com.moser.springkafka.producer;
+
+
+import br.com.moser.springkafka.People;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Juliano Moser
+ */
+@Slf4j
+@Component
+public class PeopleProducer {
+
+    private final String topicName;
+    private final KafkaTemplate<String, People>  kafkaTemplate;
+
+    public PeopleProducer(@Value("${topic.name}") String topicName, KafkaTemplate<String, People> kafkaTemplate) {
+        this.topicName = topicName;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(People people) {
+        kafkaTemplate.send(topicName, people).addCallback(
+                success -> log.info("Mensagem Enviado com sucesso!"),
+                failure -> log.error("Falha ao Enviar Mensagem!")
+        );
+    }
+}
